d2a1c8ce50cb4d92bf4d04cb9a127bee707eb890
spandana1320 <2303a51320@sru.edu.in>
2025-06-23 16:49:46 +0530

    initial commit

---
+++ ascii_values.py
@@ -0,0 +1,8 @@
+char_a='A'
+print(ord(char_a))  # ASCII value of 'A'
+ascii_value=65
+print(chr(ascii_value))  # Character for ASCII value 65
+char_b='B'
+print(ord(char_b))  # ASCII value of 'B'
+ascii_value=66
+print(chr(ascii_value))
